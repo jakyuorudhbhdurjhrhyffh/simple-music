1e937091acb7bc1f3e8e7c0f8e1af7803903ccf8
jakyuorudhbhdurjhrhyffh <47741405+jakyuorudhbhdurjhrhyffh@users.noreply.github.com>
2019-02-18 13:50:46 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-module.exports = {
-    TOKEN: 'Your bot token here', 
-    YT_API_KEY: 'Youtube API Key here', 
-    prefix: 'prefix here',
-    devs: ['your id/developers here']
+module.exports{
+    TOKEN: 'NTQ3MDA1NjY3NDI5NDQ5NzI4.D0wecA.DcEQ3aU1ZtOMoTUwm47vSMPSIy4', 
+    YT_API_KEY: 'AIzaSyCQapWI28_Um0n4AEGNVw7l3QZqhQF0cFs', 
+    prefix: '1',
+    devs: ['543892719836332043']
 }
